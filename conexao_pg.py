78c1eb1efd4e34f5eb309d8b14a02f18dbca305b
BrunaoBoldrin <33318625+BrunaoBoldrin@users.noreply.github.com>
2026-06-25 16:37:44 -0300

    Update conexao_pg.py

--- conexao_pg.py
+++ conexao_pg.py
@@ -1,19 +1,3 @@
-import os
-import psycopg2
+"""Conexão PostgreSQL compartilhada para o projeto."""
 
-
-def conectar():
-
-    return psycopg2.connect(
-
-        host=os.environ["DB_HOST"],
-
-        database=os.environ["DB_NAME"],
-
-        user=os.environ["DB_USER"],
-
-        password=os.environ["DB_PASSWORD"],
-
-        port=os.environ["DB_PORT"]
-
-    )
+from database import conectar
