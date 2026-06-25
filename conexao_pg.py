991495cc3c76da4948c3ffa215aa563e386f634a
BrunaoBoldrin <33318625+BrunaoBoldrin@users.noreply.github.com>
2026-06-25 14:57:22 -0300

    Create conexao_pg.py

---
+++ conexao_pg.py
@@ -0,0 +1,19 @@
+import os
+import psycopg2
+
+
+def conectar():
+
+    return psycopg2.connect(
+
+        host=os.environ["DB_HOST"],
+
+        port=os.environ["DB_PORT"],
+
+        database=os.environ["DB_NAME"],
+
+        user=os.environ["DB_USER"],
+
+        password=os.environ["DB_PASSWORD"]
+
+    )
